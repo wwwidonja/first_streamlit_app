ff800a0b2beea7847e8bae6c52c1378ea30feaa0
Vid Stropnik <wwwidonja@gmail.com>
2023-02-16 15:10:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,9 +35,15 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
+
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+streamlit.text('What fruit would you like to add?')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.text(f'Thanks for adding {fruit_choice})
